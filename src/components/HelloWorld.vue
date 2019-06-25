3553334c4e49c31c1e2504d1f28a04c667e3cb52
woneunjoeng <huuc10@gmail.com>
2019-06-25 14:27:59 +0900

    ./

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,42 @@
 <template>
-
+  <form id="form" @submit.prevent="submit">
+      <div>
+        <table>
+          <tbody>
+          <tr>
+            <th>ID<b>*</b></th>
+            <td>
+              <input  type="text" id='userId' name="userId">
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <th>PW</th>
+            <td>
+              <input type="text" id='userPwd' name="userPwd">
+            </td>
+            <td></td>
+          </tr>
+          </tbody>
+          <button type="submit">등록</button>
+        </table>
+      </div>
+  </form>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  methods : {
+    submit : function () {
+      let form = document.getElementById('form')
+      let formData = new FormData(form)
+      this.$http.post(this.$apiHost + '/user/login', formData)
+        .then(function (val) {
+          alert(val)
+          console.log(val)
+        })
+    }
+  }
 }
 </script>
-
+]
